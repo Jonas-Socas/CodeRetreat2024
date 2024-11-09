6a4e9cdc40487562f8bffb08e74ab66912af5018
Jonas Socas <jonas@socas.dev>
2024-11-09 10:53:34 +0000

    Initial Commit

---
+++ CodeRetreat2024V2/CodeRetreat2024V2.h
@@ -0,0 +1,18 @@
+//
+//  CodeRetreat2024V2.h
+//  CodeRetreat2024V2
+//
+//  Created by Luzma on 9/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeRetreat2024V2.
+FOUNDATION_EXPORT double CodeRetreat2024V2VersionNumber;
+
+//! Project version string for CodeRetreat2024V2.
+FOUNDATION_EXPORT const unsigned char CodeRetreat2024V2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeRetreat2024V2/PublicHeader.h>
+
+
